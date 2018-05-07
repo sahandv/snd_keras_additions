5dd13ae6ab7d0fbbec425b1fc5c871fe799cc656
sahand v <sahand.vahidnia@yahoo.com>
2018-05-08 02:20:19 +0300

    Yay! firs code!

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-snd_keras_additions
+#Sahand Keras Additions
+##snd_keras_additions
+
+This repository is intended to host a package for some simplifications in keras for lazy people like me. I knkw, keras is already too simple, but who knows, maybe there are some people looking for more simplifications! Stay tuned for more codes. 
+
+Initially the codes will be uploaded as functions, later I will pack them.
+
+Have fun! :)
---
+++ snd_data_split_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 17:51:25 2018
+
+@author: github.com/sahandv
+"""
+from sklearn.model_selection import train_test_split
+
+def snd_data_split_3(X,Y,train_proportion,test_proportion,validation_proportion):
+    size_1 = 1 - train_proportion
+    size_2 = validation_proportion/(test_proportion+validation_proportion)
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=size_1)
+    x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test, test_size=size_2)
+    return x_train,x_test,x_valid,y_train,y_test,y_valid
+
