b3205266ce848b7999be22d475e70e9d596b2c1e
sahand v <sahand.vahidnia@yahoo.com>
2018-06-08 18:31:23 +0300

    another simplification added :) , file name changed.

--- snd_base_additions.py
+++ snd_base_additions.py
@@ -5,7 +5,11 @@ Created on Thu May  3 17:51:25 2018
 
 @author: github.com/sahandv
 """
+from __future__ import print_function, division
+
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
 # =============================================================================
 # Split / Partition data with easy to understand proportions for your network 
@@ -18,3 +22,14 @@ def snd_data_split_3(X,Y,train_proportion,test_proportion,validation_proportion)
     x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test, test_size=size_2)
     return x_train,x_test,x_valid,y_train,y_test,y_valid
 
+# =============================================================================
+# Get actual and predicted Y values and return percentage of accuracy for each 
+# class.
+# =============================================================================
+def class_accuracy_percentage_calc(Y_actual,Y_prediction):
+    df_confusion = pd.crosstab(Y_actual, Y_prediction, rownames=['Actual'], colnames=['Predicted'], margins=True)
+    count_of_classess = df_confusion.All.size
+    class_accuracy_percentage = []
+    for i in range(count_of_classess-1):
+        class_accuracy_percentage.append(df_confusion[i][i]/df_confusion.All[i])
+    return class_accuracy_percentage
